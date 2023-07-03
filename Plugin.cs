eb1c5d289f201fe3cc1ed5f621601d2ff2afd872
Lunar <120229100+CoderLunar@users.noreply.github.com>
2023-07-02 21:39:42 -0600

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using System.Reflection;
+using UnityEngine;
+using HarmonyLib;
+using System.Threading.Tasks;
+using System.Linq;
+using System;
+
+namespace LunarCandycane
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private Harmony Harmony;
+
+        void Start()
+        {
+            Harmony = new Harmony(PluginInfo.GUID);
+            Harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [HarmonyPatch(typeof(VRRig), "Start", MethodType.Normal)]
+        public class Init
+        {
+            public static async void Postfix(VRRig __instance)
+            {
+                await Task.Delay(500);
+                
+                var holdables = __instance.GetComponent<BodyDockPositions>().allObjects;
+                var candycane = holdables.First(a => a.name == "CANDY CANE");
+                candycane.GetComponent<Renderer>().sharedMaterial.mainTexture = Texture2D.whiteTexture;
+                candycane.gameObject.AddComponent<ColorLerp>();
+            }
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace LunarCandycane
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "com.lunar.gorillatag.enhancedcandycane";
+        public const string Name = "enhancedcandycane";
+        public const string Version = "1.0.2";
+    }
+}
